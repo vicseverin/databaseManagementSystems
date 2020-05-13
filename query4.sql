1d8d690dc3c0dc75411d585e8fa3c5a7a3152c8e
vicseverin <40699447+vicseverin@users.noreply.github.com>
2020-05-12 18:13:34 -0700

    Delete query4.sql

--- query4.sql
+++
@@ -1,7 +0,0 @@
-SELECT DISTINCT m.memberID, m.name
-FROM Members m, CageVisits cv, Cages c, Animals a, Species s
-WHERE (m.name LIKE '%th')
-AND (m.memberID = cv.memberID)
-AND (cv.cageID = a.cageID)
-AND (a.speciesID = s.speciesID)
-AND (s.speciesName = 'lion');
